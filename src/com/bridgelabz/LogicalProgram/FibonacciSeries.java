a0a8ba275e38ff17655a58b9af99fb49d29d708e
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-04-13 05:04:04 +0530

    Added: Checking number for a perfect Number

--- src/com/bridgelabz/LogicalProgram/FibonacciSeries.java
+++ src/com/bridgelabz/LogicalProgram/FibonacciSeries.java
@@ -20,7 +20,7 @@ public class FibonacciSeries {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter a Number: ");
+		System.out.print("Enter a Number");
 		int num = sc.nextInt();
 		fibonacciSeries(num);
 	}
