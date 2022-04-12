b6c61cc6f3277d71f684676ca255208afbb5cb0c
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-04-13 04:52:11 +0530

    Added: Program for Fibonacci Number

--- src/com/bridgelabz/LogicalProgram/FibonacciSeries.java
+++ src/com/bridgelabz/LogicalProgram/FibonacciSeries.java
@@ -20,7 +20,7 @@ public class FibonacciSeries {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("Enter a num :- ");
+		System.out.print("Enter a Number: ");
 		int num = sc.nextInt();
 		fibonacciSeries(num);
 	}
